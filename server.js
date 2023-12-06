947b2c3ecfe44a495bc27e329f66e2bbc038bc71
Escher WD <escherwd@icloud.com>
2023-12-05 19:36:42 -0800

    page layout

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 
 var express = require('express')
-var app = express()
+var port = process.env.PORT || 3000
 
+// Create the express server
+var app = express()
+// Use the static middleware
 app.use(express.static('static'))
 
-var port = process.env.PORT || 3000
+
+// Listen on port
 app.listen(port, function () {
     console.log("== Server is listening on port", port)
 })
