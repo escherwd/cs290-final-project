e7a024ef4ca1c61abd1c42da4b6fba9d175fc86e
Escher WD <escherwd@icloud.com>
2023-12-07 15:25:43 -0800

    json storage

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 
+const fs = require('fs');
+const path = require('path');
 var express = require('express')
 var port = process.env.PORT || 3000
 
@@ -6,9 +8,46 @@ var port = process.env.PORT || 3000
 var app = express()
 // Use the static middleware
 app.use(express.static('static'))
+app.use(express.json());
+
+app.post('/save', function (req, res) {
+    let name = req.body['name']
+    let data = req.body['data']
+
+    let file = path.join(__dirname, 'storage', `${name}.json`);
+
+    fs.writeFileSync(file, JSON.stringify(data))
+    res.end()
+})
+
+app.get('/files', function (req, res) {
+    let name = req.body['name']
+    let data = req.body['data']
+
+    let files = fs.readdirSync(path.join(__dirname, 'storage'))
+        .map(file => file.replace('.json', ''))
+
+    res.send({ files: files })
+})
+
+app.get('/data', function (req, res) {
+    let name = req.query.name
+
+    let file = path.join(__dirname, 'storage', `${name}.json`)
+
+    if (!fs.existsSync(file)) {
+        res.status(404).send({ 'error': 'File not found' })
+        return
+    }
+
+    let data = fs.readFileSync(file)
+
+    res.send(JSON.parse(data))
+})
 
 
 // Listen on port
 app.listen(port, function () {
     console.log("== Server is listening on port", port)
 })
+
