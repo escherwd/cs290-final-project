208c451cfd2d15bf7a4e1b03601c64f4be30bd5d
Owner <barnetth@oregonstate.edu>
2023-12-07 20:54:25 -0800

    Added dimensions to walls. Added button to add from svg.

--- static/canvas_test.js
+++ static/canvas_test.js
@@ -1,8 +1,11 @@
 // import {fabric} from 'fabric'
 // var fabric = require('fabric')
 var canvas = this.__canvas = new fabric.Canvas('c');
+canvas.preserveObjectStacking = true;
 // create a rect object
 var deleteIcon = "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'%3E%3Csvg version='1.1' id='Ebene_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='595.275px' height='595.275px' viewBox='200 215 230 470' xml:space='preserve'%3E%3Ccircle style='fill:%23F44336;' cx='299.76' cy='439.067' r='218.516'/%3E%3Cg%3E%3Crect x='267.162' y='307.978' transform='matrix(0.7071 -0.7071 0.7071 0.7071 -222.6202 340.6915)' style='fill:white;' width='65.545' height='262.18'/%3E%3Crect x='266.988' y='308.153' transform='matrix(0.7071 0.7071 -0.7071 0.7071 398.3889 -83.3116)' style='fill:white;' width='65.544' height='262.179'/%3E%3C/g%3E%3C/svg%3E";
+
+var ppi = 5 // Pixles per inch
 
 var img = document.createElement('img');
 img.src = deleteIcon;
@@ -23,23 +26,28 @@ var grid = 50;
 
 // create grid
 
-var canvasWidth = canvas.getWidth();
-var canvasHeight = canvas.getHeight();
-console.log("==CanvasWidth:", canvasWidth)
-console.log("==CanvasHeight:", canvasHeight)
+function createGrid(){
+  var canvasWidth = canvas.getWidth();
+  var canvasHeight = canvas.getHeight();
+  console.log("==CanvasWidth:", canvasWidth)
+  console.log("==CanvasHeight:", canvasHeight)
 
-for (var i = 0; i < (canvasWidth / grid); i++) {
-  canvas.add(new fabric.Line([i * grid, 0, i * grid, canvasHeight], {
-    stroke: '#ccc',
-    selectable: false
-  }));
+  // gridGroup = new fabric.Group(); 
+  for (var i = 0; i < (canvasWidth / grid); i++) {
+    canvas.add(new fabric.Line([i * grid, 0, i * grid, canvasHeight], {
+      stroke: '#ccc',
+      selectable: false,
+      preserveObjectStacking: true,
+    }));
+  }
+  for (var i = 0; i < (canvasHeight / grid); i++) {
+    canvas.add(new fabric.Line([0, i * grid, canvasWidth, i * grid], {
+      stroke: '#ccc',
+      selectable: false
+    }))
+  }
 }
-for (var i = 0; i < (canvasHeight / grid); i++) {
-  canvas.add(new fabric.Line([0, i * grid, canvasWidth, i * grid], {
-    stroke: '#ccc',
-    selectable: false
-  }))
-}
+createGrid();
 
 function Add() {
 var rect = new fabric.Rect({
@@ -57,6 +65,17 @@ canvas.add(rect);
 canvas.setActiveObject(rect);
 }
 
+function addSVG(){
+fabric.loadSVGFromURL('./lib/svg/bed.svg', function(objects, options) {
+  var obj = fabric.util.groupSVGElements(objects, options);
+  obj.scale(2);
+  obj.left = 100;
+  obj.top = 100;
+  console.log(obj)
+  canvas.add(obj);
+});
+}
+
 fabric.Object.prototype.controls.deleteControl = new fabric.Control({
 x: 0.5,
 y: -0.5,
@@ -67,7 +86,7 @@ render: renderIcon,
 cornerSize: 24
 });
 
-Add();
+// Add();
 
 function deleteObject(eventData, transform) {
     var target = transform.target;
@@ -113,70 +132,32 @@ function drawWall(){
 
 }
 
-function drawEndpoint(event){
-  console.log(event)
-  var pointer = canvas.getPointer(event);
-  var posX = pointer.x;
-  var posY = pointer.y;
-  var radius = 10
-  console.log(posX, posY)
-  endpoint = new fabric.Circle({
-    radius: radius, 
-    fill: 'green',
-    left: posX - (radius),
-    top: posY - (radius),
-
-  })
-  canvas.add(endpoint)
-  return {posX, posY}
-}
-
-// canvas.on('mouse:down', (event) => drawEndpoint(event))
-// canvas.on('mouse:up', (event) => drawEndpoint(event))
-
-// var start = canvas.on('mouse:down', (event) => drawEndpoint(event))
-// var end = canvas.on('mouse:up', (event) => drawEndpoint(event))
-// console.log(start)
-// console.log(end)
-
-// canvas.add(new fabric.Line([start.posX, start.posY, end.posX, end.posY], {
-//   stroke: 'red'
-// }))
-
-// from http://fabricjs.com/stickman demo 
-
-// function makeCircle(left, top, line1, line2, line3, line4) {
-//   var c = new fabric.Circle({
-//     left: left,
-//     top: top,
-//     strokeWidth: 5,
-//     radius: 12,
-//     fill: '#fff',
-//     stroke: '#666'
-//   });
-//   c.hasControls = c.hasBorders = false;
-
-//   c.line1 = line1;
-//   c.line2 = line2;
-//   c.line3 = line3;
-//   c.line4 = line4;
-
-//   return c;
-// }
-
-// function makeLine(coords) {
-//   return new fabric.Line(coords, {
-//     fill: 'red',
-//     stroke: 'red',
-//     strokeWidth: 5,
-//     selectable: false,
-//     evented: false,
-//   });
-// }
-
 var prevCircle = null;
 
+function lineMidpoint(line){
+  var x = (line.x1 + line.x2) /2;
+  var y = (line.y1 + line.y2) /2; 
+  return [x, y];
+}
+function lineLength(line){
+  var length = (((line.x1 + line.x2)/2)^2+((line.y1 + line.y2)/2)^2)^0.5
+  length = length/ppi; 
+  return length;
+}
+
+function updateDimPos(line){
+  var midpoint = lineMidpoint(line);
+  var length = lineLength(line).toString();
+  length = length + '"'
+  line.dim.set({
+    left: midpoint[0],
+    top: midpoint[1],
+    text: length,
+  })
+}
+
 function linkCircle(e, circle, prevCircle){
+  console.log("linkCircle");
   var line = new fabric.Line([
     prevCircle.left + circle.radius, 
     prevCircle.top + circle.radius,
@@ -189,9 +170,34 @@ function linkCircle(e, circle, prevCircle){
     selectable: false,
     evented: false,
   });
+
+  var length = (((line.x1 + line.x2)/2)^2+((line.y1 + line.y2)/2)^2)^0.5
+  var midPoint = lineMidpoint(line);
+
+  console.log("==Midpoint", midPoint)
+  console.log('==Length: ', length);
+
+  var dim = new fabric.Text(length.toString(), {
+    fontSize: 10, 
+    left: midPoint[0],
+    top: midPoint[1],
+    selectable: false,
+  })
+
+  // var lineGroup = new fabric.Group([line, dim],{
+  //   selectable: false,
+
+  // })
+  line.dim = dim;
+  updateDimPos(line);
   // circle.lineTo = prevCircle.lineFrom;
   prevCircle.lineFrom = line;
   canvas.add(line);
+  canvas.add(line.dim);
+  // console.log(line.group);
+  // line.sendToBack(); 
+  circle.bringToFront();
+  prevCircle.bringToFront();
   return line; 
 }
 
@@ -213,7 +219,6 @@ function makeCircle(e, prevCircle){
     circle.next = null; 
     circle.lineFrom = null;
     // circle.lineTo = prevCircle.lineFrom;
-
     return circle;
 
 }
@@ -269,20 +274,37 @@ canvas.on('mouse:up', (e) => {
       canvas.add(circle);
     }
     firstWall = false;
+    // canvas.preserveObjectStacking = false;
+    // circle.bringToFront();
+    // canvas.preserveObjectStacking = true;
+    // canvas.bringToFront(circle);
     canvas.renderAll();
   }
 })
 
+
+
 canvas.on('object:moving', (e) => {
   var o = e.target;
+  console.log("Dragging Object:", o)
   if (o.type != null && o.type === 'circle'){
+    console.log("___Dragging Circle");
     var radius = o.radius;
     if (o.lineTo != null){
+      console.log("Setting lineTo end");
       o.lineTo.set({'x2': o.left + radius, 'y2': o.top + radius})
+      updateDimPos(o.lineTo)
     }
     if (o.lineFrom != null){
+      console.log("Setting lineFrom end")
       o.lineFrom.set({'x1': o.left + radius, 'y1': o.top + radius})
+      updateDimPos(o.lineFrom)
     }
   }
 }
 )
+
+function clearAll(){
+  canvas.clear();
+  createGrid();
+}
