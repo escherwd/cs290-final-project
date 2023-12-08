cfbe3961cf42047b1899e8f8e527b9abc265788f
jacksjud <137832064+jacksjud@users.noreply.github.com>
2023-12-07 18:52:28 -0800

    some SVG stuff
    Not much, mainly SVG directory

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
-
+const path = require('path');
 var express = require('express')
-var port = process.env.PORT || 3000
+var exphbs = require("express-handlebars")
+var fabric = require("fabric").fabric;
+
 
 // Create the express server
 var app = express()
+var port = process.env.PORT || 3000 // PORT
+
+
+
+//app.engine("handlebars", exphbs.engine( { defaultLayout: "main"}));
+//app.set("view engine", "handlebars");
+
 // Use the static middleware
-app.use(express.static('static'))
+//app.use(express.static('static'))
 
+app.use(express.static('static'));
 
-// Listen on port
+console.log("== path: " + path);
+
+app.get('/SVGs/:filename', function(req, res){
+    
+})
+
+app.get("*", function(req, res, next) {
+
+})
+
 app.listen(port, function () {
     console.log("== Server is listening on port", port)
 })
