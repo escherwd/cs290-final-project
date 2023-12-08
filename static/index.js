9a134f702682de17b8f6deca7f6106b230718d46
jacksjud <137832064+jacksjud@users.noreply.github.com>
2023-12-08 03:35:06 -0800

    Fairly Large Update
    updates objects data and adds a few more buttons. also adds reset functionality and adds delete button to each new object

--- static/index.js
+++ static/index.js
@@ -1,53 +1,147 @@
 /* *********************************      Global Definitions       ******************************* */ 
 
 const canvas = new fabric.Canvas('canvas2'); // Reference to canvas element
+var deleteIcon = "/SVGs/delete-button.svg"
+var cloneIcon =  "/SVGs/copy-button.svg"
 
-fabric.Object.prototype.transparentCorners = false; // no transparent corners
-fabric.Object.prototype.cornerStyle = "circle"; // circle corners so it's visible to users
-fabric.Object.prototype.cornerColor = "blue"; // circles are blue
+var deleteImg = document.createElement('img');
+deleteImg.src = deleteIcon;
+
+var clonedImg = document.createElement('img');
+cloneIcon.src = cloneIcon;
 
 const palletteElements = document.querySelectorAll(".pallette-grid"); // gets all elements w/ buttons that are rendered
-for(const element of palletteElements){
+for(const element of palletteElements){ // adds event listeners to all layout and object buttons 
     console.log("== palletteElements:" + element);
     for(const child of element.children) {
         child.addEventListener("click", createObject);
     }
 }
 
+const resetButton = document.getElementById("reset-button")
+resetButton.addEventListener("click", resetButtonHandler)
+
 /* *********************************              Base Functionality             ******************************* */ 
 
-function svgDocument(path){
-    return readFileSync(path);
+
+fabric.Object.prototype.transparentCorners = false; // no transparent corners
+fabric.Object.prototype.cornerStyle = "circle"; // circle corners so it's visible to users
+fabric.Object.prototype.cornerColor = "blue"; // circles are blue
+
+fabric.Object.prototype.controls.deleteControl = new fabric.Control({
+    x: 0.5,
+    y: -0.5,
+    offsetY: -16,
+    offsetX: 16,
+    cursorStyle: 'pointer',
+    mouseUpHandler: deleteObject,
+    render: renderIcon(deleteImg),
+    cornerSize: 24 
+});
+
+// fabric.Object.prototype.controls.cloneButton = new fabric.Control({
+//     x: -0.5,
+//     y: -0.5,
+//     offsetY: -16,
+//     offsetX: -16,
+//     cursorStyle: 'pointer',
+//     mouseUpHandler: cloneObject,
+//     render: renderIcon(clonedImg),
+//     cornerSize: 24
+// });
+
+function renderIcon(icon) {
+    return function renderIcon(ctx, left, top, styleOverride, fabricObject) {
+        var size = this.cornerSize;
+        ctx.save();
+        ctx.translate(left, top);
+        ctx.rotate(fabric.util.degreesToRadians(fabricObject.angle));
+        ctx.drawImage(icon, -size/2, -size/2, size, size);
+        ctx.restore();
+    }
 }
+
+function deleteObject(eventData, transform) {
+    var target = transform.target;
+    var canvas = target.canvas;
+    canvas.remove(target);
+    canvas.requestRenderAll();
+}
+
+// function cloneObject(eventData, transform) {
+//     var target = transform.target;
+//     var canvas = target.canvas;
+//     target.clone(function(cloned) {
+//         cloned.left += 10;
+//         cloned.top += 10;
+//         canvas.add(cloned);
+//     });
+// }
 
 /* *********************************      Tool Ribbon Place Implementation       ******************************* */ 
 
-/* 
-All scalable, dependent on user's click and drag
-*/
 
-// need buttons in tool bar
+// function deleteButtonHandler() {
+//     console.log("== deleteButtonHandler called")
+//     var activeObjects = canvas.getActiveObjects()
+//     console.log("== Check for active objects being registered: " + activeObjects);
+    
+//     canvas.renderAll();
+    
+// }
+
+
+
+// var floorButton = document.getElementById('create-floor');
+// floorButton.addEventListener('click', createFloor);
+
+// function createFloor() {
+
+
+//     // Create a resizable floor object
+//     var floor = new fabric.Rect({
+//     left: 100,
+//     top: 100,
+//     width: 200,
+//     height: 150,
+//     fill: 'lightgray',
+//     selectable: true,
+//     hasControls: true,
+//     hasBorders: true,
+//     lockScalingFlip: true,
+//     lockUniScaling: true,
+//     cornerSize: 10,
+//     transparentCorners: false,
+//     cornerStyle: 'circle',
+//     scalable: true,
+//     });
+
+//     // Add the floor object to the canvas
+//     canvas.add(floor);
+
+//     // Render the canvas
+//     canvas.renderAll();
+
+// }
+
+
+
+
+
+/* *********************************      Insert Objects Implementation       ******************************* */ 
+
 
 function createObject(event){
-    var id = event.target.id
-    console.log("== event target id:" + id)
-    fabric.loadSVGFromURL('/SVGs/' + id + '.svg' ,function(object, options ) {
-        var object = fabric.util.groupSVGElements(object, options);
-        canvas.add(object);
-    })
-}
-
-
-function deleteButton() {
-
-}
-
-
-/* *********************************      Objects to Place Implementation       ******************************* */ 
-
-function insertPremadeObject() {
-    // need html code for object buttons implemented for this to properly be implemented
-    // also need code for premade canvas objects 
+    var button = event.target.closest('button'); // checks if clicked element or it's closest ancestor is button
+    if(button){
+        var id = button.id
+        console.log("== event target id:" + id)
+        fabric.loadSVGFromURL('/SVGs/' + id + '.svg' ,function(object, options ) {
+            var objectSVG = fabric.util.groupSVGElements(object, options);
+            canvas.add(objectSVG);
+        })
+    }
+    
 }
 
 /* *********************************            Menu Implementation              ******************************* */ 
@@ -57,8 +151,9 @@ function saveButtonHandler() {
 }
 
 
-function resetButtonHandler() {
-// need button in top menu
+function resetButtonHandler(event) {
+// Clears canvas entirely
+    canvas.clear();
 }
 
 function generatePDF() {
@@ -75,5 +170,3 @@ function generatePDF() {
 
     html2pdf().set(options).from(element).save(); // call function to export to pdf
 }
-
-/* *********************************          Work Area Implementation           ******************************* */ 
